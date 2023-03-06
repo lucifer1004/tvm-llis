1db0b8c7820eba46f9da45e8f16283315a27b283
Kelvin Ng <nkwkelvin@gmail.com>
2023-03-06 16:24:48 -0500

    Update onnx2tvm

--- my_tests/onnx2tvm.py
+++ my_tests/onnx2tvm.py
@@ -1,24 +1,26 @@
 import onnx
+import numpy as np
 import tvm
 from tvm import te
 import tvm.relay as relay
+from tvm.contrib.download import download_testdata
 import sys
-
-def change_batch_size(model, dim):
-    inputs = model.graph.input
-    for input in inputs:
-        dim1 = input.type.tensor_type.shape.dim[0]
-        dim1.dim_value = dim
 
 model_path = sys.argv[1] # path to the onnx model
 output_path = sys.argv[2] # path to output the tvm-compiled model
-target = sys.argv[3] # cuda or cuda_llis
-batch_size = int(sys.argv[4]) # batch size
+target = sys.argv[3] # cuda or cuda -llis_flag=[1|3|5]
+input_dims = tuple(int(x) for x in sys.argv[4:]) # e.g., `1 3 224 224` for mobilenet
 
 onnx_model = onnx.load(model_path)
-change_batch_size(onnx_model, batch_size)
 
-mod, params = relay.frontend.from_onnx(onnx_model)
+input_names_all = [node.name for node in onnx_model.graph.input]
+input_initializer =  [node.name for node in onnx_model.graph.initializer]
+input_names = list(set(input_names_all)  - set(input_initializer))
+
+input_name = input_names[0]
+
+shape_dict = {input_name: input_dims}
+mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
 
 opt_level = 3
 with tvm.transform.PassContext(opt_level=opt_level):
